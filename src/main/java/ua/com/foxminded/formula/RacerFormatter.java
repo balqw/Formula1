01796d6c237c201ee33da8855ef6074d07d820ba
balqw <balqw777@gmail.com>
2020-08-14 13:42:32 +0300

    fix sort

--- src/main/java/ua/com/foxminded/formula/RacerFormatter.java
+++ src/main/java/ua/com/foxminded/formula/RacerFormatter.java
@@ -1,15 +1,19 @@
 package ua.com.foxminded.formula;
 
 import java.time.LocalTime;
+import java.util.Collections;
 import java.util.List;
-
+import org.apache.commons.lang3.StringUtils;
 public class RacerFormatter {
 
     public void formatted(List<Racer> source){
+        Collections.sort(source);
         int nameMaxLength=0;
         int teamMaxLength=0;
+        int maxLengthLine;
+        int otherSymbols=17;
         String lapTime;
-
+        String underLine = "_";
 
         for(Racer racer : source){
             if(racer.getName().length()>nameMaxLength)
@@ -17,14 +21,19 @@ public class RacerFormatter {
             if(racer.getTeam().length()>teamMaxLength)
                 teamMaxLength=racer.getTeam().length();
         }
+        maxLengthLine = nameMaxLength+teamMaxLength+otherSymbols;
 
         for (int i = 0;i <source.size();i++){
             lapTime = LocalTime.ofNanoOfDay(source.get(i).getLapTime().toNanos()).toString();
+
             if(i==15)
             {
-                System.out.println("___________________________________________________________");
+                System.out.println( StringUtils.repeat(underLine,maxLengthLine));
             }
-            System.out.printf("%2d.%-"+nameMaxLength+"s%s%-"+teamMaxLength+"s%s%s%n",i+1,source.get(i).getName(),"|",source.get(i).getTeam(),"|",lapTime);
+            System.out.printf("%2d.%-"+nameMaxLength+"s%" +
+                    "s%-"+teamMaxLength+"s%s%" +
+                    "s%n",
+                    i+1,source.get(i).getName(),"|",source.get(i).getTeam(),"|",lapTime);
         }
     }
 
