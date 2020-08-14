5df6c77483ea178884838aded8ef1800c200a67f
balqw <balqw777@gmail.com>
2020-08-14 10:27:41 +0300

    fix names

--- src/main/java/ua/com/foxminded/formula/MainApp.java
+++ src/main/java/ua/com/foxminded/formula/MainApp.java
@@ -10,23 +10,23 @@ public class MainApp {
 
        FileReader fileReader = new FileReader();
 
-       File start_file = new File("src/main/resources/start.log");
-       File end_file = new File("src/main/resources/end.log");
+       File startFile = new File("src/main/resources/start.log");
+       File endFile = new File("src/main/resources/end.log");
        File abbreviations_file = new File("src/main/resources/abbreviations.txt");
 
 
-       List<String>start_log_file = fileReader.read(start_file);
-       List<String>end_log_file = fileReader.read(end_file);
-       List<String>abbreviation_file = fileReader.read(abbreviations_file);
+       List<String>startLogList = fileReader.read(startFile);
+       List<String>endLogList = fileReader.read(endFile);
+       List<String>abbreviationList = fileReader.read(abbreviations_file);
 
 
 
        RacerBuilder racerBuilder = new RacerBuilder();
-       List<Racer>kkk = racerBuilder.createRacers(start_log_file,end_log_file,abbreviation_file );
+       List<Racer>racers = racerBuilder.createRacers(startLogList,endLogList,abbreviationList );
 
 
       RacerFormatter formatter = new RacerFormatter();
-      formatter.formatted(kkk);
+      formatter.formatted(racers);
 
    }
 }
